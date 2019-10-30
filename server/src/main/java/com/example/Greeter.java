81448d99976415f33464cbbc935029d20a5a2731
chps78 <52750016+chps78@users.noreply.github.com>
2019-10-30 10:31:26 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,6 +6,8 @@ public class Greeter {
   public Greeter() {
 
   }
+  // @param someone
+  // @return string
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
