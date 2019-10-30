111db71e4375f20c8df31e327bc6510a9aad2a98
chps78 <52750016+chps78@users.noreply.github.com>
2019-10-30 10:50:48 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,20 @@
 package com.example;
 
-// Class greter
+/**
+* Class greter
+*/
 public class Greeter {
 
-  // Constructeur Greeter
+/**
+* Constructeur greter
+*/
   public Greeter() {
 
   }
-  // @param someone - someone
-  // @return string
+  /**
+  *@param someone - someone
+  *@return string
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
